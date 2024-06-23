ff60f2709d6c5a6e495de64bb1269203a7a87384
mylkner <mylofaulkner27@gmail.com>
2024-06-23 04:41:04 +0200

    update base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "https://mylkner.github.io/todo_app",
+    base: "/todo_app",
 });
