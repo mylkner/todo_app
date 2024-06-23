b67aa404b121011d6d80f64912649dbd8c009ea2
mylkner <mylofaulkner27@gmail.com>
2024-06-23 04:36:29 +0200

    wrong folder for gh-pages and added base URL to vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        proxy: {
-            "/api": {
-                target: "http://localhost:3000",
-            },
-        },
-    },
+    base: "https://mylkner.github.io/todo_app",
 });
